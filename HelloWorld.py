9458da2312c30b215c0715c7651d39456c07cdb7
Jordin <jordin@LAPTOP-C9F4VOB3.localdomain>
2020-02-24 13:53:15 +0100

    added a main method to test invoke the main

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,16 @@
 print("hello")
 print("World")
 print("Modified on Yoga")
+
+print("Hello")
+
+print("__name__ value: ", __name__)
+
+
+def main():
+    print("python main function")
+    print("Test passed since main method is evoked ")
+
+
+if __name__ == '__main__':
+    main()
